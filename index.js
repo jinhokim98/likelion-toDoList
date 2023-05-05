4f1fdb3d431481317c802186b4d96a03efa7284f
김진호 <rlawlsgh1227@gmail.com>
2023-05-05 16:19:47 +0900

    feat: Save and delete data to localStorage

--- index.js
+++ index.js
@@ -1,23 +1,67 @@
 const inputBox = document.querySelector('#input_');
 const list = document.querySelector('.showList');
 
-let listNumber = 0;
+// 고유키 만들기
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+}
 
-function showToDo(event) {
+function init() {
+    let localStorageKeys = [];
+    let dataList = []; 
+
+    for (let i = 0; i<localStorage.length; i++) {
+        localStorageKeys.push(localStorage.key(i));
+    }
+
+    localStorageKeys.map(item => {
+        dataList.push(JSON.parse(localStorage.getItem(item)));
+    })
+
+
+    dataList.map(item => {
+        const appendItem = document.createElement('li');
+    
+        const createItem = `
+        <div id='row${item.key}' class='row'>
+            <input id='${item.key}' type='checkbox' class='checkbox live' onclick='setLineThrough(this)'/>
+            <span id='desc${item.key}' class='desc'>${item.desc}</span>
+        <div>
+        `;
+
+        appendItem.innerHTML = createItem;
+        list.appendChild(appendItem);
+        listStyle();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+function addList(event) {
     event.preventDefault();
-
+    
+    const uuidKey = uuidv4();
     const toDo = inputBox.value;
     const appendItem = document.createElement('li');
     const item = `
-    <div id='row${listNumber}' class='row'>
-        <input id='${listNumber}' type='checkbox' class='checkbox live' onclick='setLineThrough(this)'/>
-        <span id='desc${listNumber}' class='desc'>${toDo}</span>
+    <div id='row${uuidKey}' class='row'>
+        <input id='${uuidKey}' type='checkbox' class='checkbox live' onclick='setLineThrough(this)'/>
+        <span id='desc${uuidKey}' class='desc'>${toDo}</span>
     <div>
     `;
     appendItem.innerHTML = item;
     list.appendChild(appendItem);
     listStyle();
-    listNumber++;
+    
+    // save to local storige
+    const data = {
+        key: uuidKey,
+        desc: toDo
+    }
+    localStorage.setItem(`${data.key}`, JSON.stringify(data));
 };
 
 function showToday() {
@@ -69,12 +113,22 @@ function setLineThrough(idx) {
 
 function deleteList(event) {
     event.preventDefault();
+    let deleteKey = [];
 
     const allCheckbox = document.querySelectorAll('.checkbox');
     const checkedCheckbox = [...allCheckbox]
         .filter(checkbox => checkbox.checked === true)
         .map(item => item.id)
-        .map(id => document.querySelector(`#row${id}`).parentNode);
 
-    checkedCheckbox.map(item => list.removeChild(item));
+    // 삭제할 대상의 id를 저장한다.
+    checkedCheckbox.map(id => deleteKey.push(id));
+
+    const rowItem = checkedCheckbox
+    .map(id => document.querySelector(`#row${id}`).parentNode);
+
+    // View에서 지운다.
+    rowItem.map(item => list.removeChild(item));
+
+    // delete from local storige
+    deleteKey.map(id => localStorage.removeItem(id));
 }
